6214dccf2aab65b3aa2290c7c0b25e44f10027f9
Ankan Poddar <friendyankan@gmail.com>
2018-04-26 11:04:09 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-void swap(int*, int*);
 
 class MaxHeap {
 
@@ -10,72 +9,86 @@ class MaxHeap {
 	int maximum_size;
 	int heap_size;
 
-	int parent(int i) {
+	int parent(const int& i) {
 		return (i-1)/2;
 	}
 
-	int leftChild(int i) {
+	int leftChild(const int& i) {
 		return (2*i) + 1;
 	}
 
-	int rightChild(int i) {
+	int rightChild(const int& i) {
 		return (2*i) + 2;
 	}
 
+	void swap(int& num1, int& num2) {
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
+
 public:
-	MaxHeap(int max_size) {
+	MaxHeap(const int& max_size) {
 		heap_size = 0;
 		maximum_size = max_size;
 		heap_array = new int[max_size];
 	}
-
-	void maxHeapInsert(int num) {
-		if (heap_size == maximum_size) {
-			cout << "Can't add more elements, heap is full" << endl;
-			return;
-		}
-
-		heap_size++;
-		int i = heap_size - 1;
-		heap_array[i] = num;
-
-		while (i != 0 && heap_array[parent(i)] < heap_array[i]) {
-			swap(&heap_array[i], &heap_array[parent(i)]);
-			i = parent(i);
-		}
+	MaxHeap(const MaxHeap& other) {
+		heap_size = other.heap_size;
+		maximum_size = other.maximum_size;
+		heap_array = new int[maximum_size];
+		for (int i = 0 ; i < heap_size; ++i)
+			heap_array[i] = other.heap_array[i];
 	}
-
-	void maxHeapDelete(int index)  {
-		if (heap_size == 0 || index > heap_size) {
-			cout << "Can't delete a non-existent element" << endl;
-			return;
-		}
-
-		swap(&heap_array[index], &heap_array[--heap_size]);
-		int i = index;
-
-		while (leftChild(i) <= heap_size && (heap_array[i] < heap_array[leftChild(i)] || heap_array[i] < heap_array[rightChild(i)])) {
-			int swappable = heap_array[leftChild(i)] > heap_array[rightChild(i)] ? leftChild(i) : rightChild(i);
-			swap(&heap_array[swappable], &heap_array[i]);
-			i = swappable;
-		}	
+	~MaxHeap() {
+		delete[] heap_array;
 	}
-
-	void showHeap() {
-		int i = 0;
-		cout << endl << "Heap: ";
-		while (i < heap_size) {
-			cout << heap_array[i++] << " ";
-		}
-		cout << endl;
-	}
+	void maxHeapInsert(const int&);
+	void maxHeapDelete();
+	void showHeap();
 };
 
-void swap(int *num1, int *num2) {
-	int temp = *num1;
-	*num1 = *num2;
-	*num2 = temp;
+void MaxHeap :: maxHeapInsert(const int& num) {
+	if (heap_size == maximum_size) {
+		cout << "Can't add more elements, heap is full" << endl;
+		return;
+	}
+
+	heap_size++;
+	int i = heap_size - 1;
+	heap_array[i] = num;
+
+	while (i != 0 && heap_array[parent(i)] < heap_array[i]) {
+		swap(heap_array[i], heap_array[parent(i)]);
+		i = parent(i);
+	}
 }
+
+void MaxHeap :: maxHeapDelete()  {
+	if (heap_size == 0) {
+		cout << "Can't delete a non-existent element" << endl;
+		return;
+	}
+
+	swap(heap_array[0], heap_array[--heap_size]);
+	int i = 0;
+
+	while (leftChild(i) <= heap_size && (heap_array[i] < heap_array[leftChild(i)] || heap_array[i] < heap_array[rightChild(i)])) {
+		int swappable = heap_array[leftChild(i)] > heap_array[rightChild(i)] ? leftChild(i) : rightChild(i);
+		swap(heap_array[swappable], heap_array[i]);
+		i = swappable;
+	}	
+}
+
+void MaxHeap :: showHeap() {
+	int i = 0;
+	cout << endl << "Heap: ";
+	while (i < heap_size) {
+		cout << heap_array[i++] << " ";
+	}
+	cout << endl;
+}
+
 
 main(void) {
 	int size, elements, num;
